c1eef955758e28bd5aedbc8443e419023f67a78e
Máté Koncz <matthewkoncy@gmail.com>
2023-04-26 18:59:56 +0200

    Add lesson and homework 11 (#12)

--- 11-lesson-return/homework/19.js
+++ 11-lesson-return/homework/19.js
@@ -1,11 +1,11 @@
-console.log("Exercise 3");
-// Write a JavaScript function to compute the sum of the two given numbers.
-// If the two values are the same, then return 0. 
 
-function sum(n1, n2) {
-    if (n1 == n2) {
-        return 0;
-    }
-    return n1 + n2;
-}
-console.log(sum(3, 4))
+console.log("Exercise 2");
+/* 
+  Write a JavaScript function to convert temperatures from Fahrenheit to Celsius. 
+  Formula: c/5 = (f-32)/9  
+    (where c = "temperature in Celsius" and f = "temperature in Fahrenheit")
+  Use console log. Return is not needed.
+  Expected Output:
+  toCelsius(140) => "140°F is 60°C"
+  toCelsius(45) => "45°F is 7.222222222222222°C"
+*/
--- 11-lesson-return/homework/21.js
+++ 11-lesson-return/homework/21.js
@@ -1,3 +1,4 @@
 console.log("Exercise 5");
-// Write a JavaScript function to create a new string adding "Py" to the beginning of a given string parameter.
+// Write a JavaScript function to create a new string adding "Py" to
+// the beginning of a given string parameter.
 // If the given string begins with "Py" then return the original string.
