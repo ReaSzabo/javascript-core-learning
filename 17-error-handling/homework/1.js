4ff14f939f23089a7228681680a6a17212af091d
Máté Koncz <matthewkoncy@gmail.com>
2023-09-23 09:18:17 +0200

    Add lesson and homework 17 (#12)

---
+++ 17-error-handling/homework/1.js
@@ -0,0 +1,9 @@
+// Írj egy függvényt! 
+// A függvény paramétere egy szám tömb.
+// A visszatérési értéke a paraméterként kapott tömb növekvő sorrendbe rendezve.
+// A feladat végrehajtásához különböző rendező algoritmusok léteznek. Ezeknek utánanézhetsz az interneten.
+// Kérlek került a JavaScript beépített .sort() metódusát! :D
+
+var testArray = [3, 2, 6, 9];
+var testArray2 = [11, 55, 3, 22];
+var testArray3 = [1000, 500, 3000, 220];
